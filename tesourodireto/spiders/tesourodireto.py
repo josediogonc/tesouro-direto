dd61f204f9000c9aec05814c14066f7fe3e58537
Leonardo Gregianin <leogregianin@gmail.com>
2016-12-10 17:22:55 -0300

    Atualização do scraper após atualização do site do Tesouro Direto

--- tesourodireto/spiders/tesourodireto.py
+++ tesourodireto/spiders/tesourodireto.py
@@ -10,15 +10,22 @@ class TesouroDiretoSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        list_resp = response.xpath('//tr/td/text()').extract()
+        list_resp = response.xpath("//tr[contains(@class, 'camposTesouroDireto')]/td/text()").extract()
         i = 0
+        contador = 0
 
-        for index in xrange((len(list_resp)/6)):
-            Desc        = list_resp[i+1]
-            Vencto      = list_resp[i+2]
-            TaxaCompra  = list_resp[i+3]
-            TaxaVenda   = list_resp[i+4]
-            PrecoCompra = list_resp[i+5]
-            PrecoVenda  = list_resp[i+6]
-            print Desc, Vencto, TaxaCompra, TaxaVenda, PrecoCompra, PrecoVenda
-            i+=6
+        for index in xrange((len(list_resp)/5)):
+            Titulo = list_resp[i+0]
+            Vencto = list_resp[i+1]
+            TaxaRendimentoAa = list_resp[i+2]
+
+            print('Titulo: %s Vencimento: %s Taxa a.a: %s \n' % (Titulo, Vencto, TaxaRendimentoAa))
+            i+=5
+
+            if (Titulo.strip() == "Tesouro IPCA+ 2019 (NTNB Princ)"):
+                contador+=1
+            
+            if contador == 2:
+                break
+
+
